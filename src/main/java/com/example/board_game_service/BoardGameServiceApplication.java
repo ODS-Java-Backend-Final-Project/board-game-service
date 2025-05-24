b94186dc83e111ece31e4ffa24d8c10d209b7dda
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-24 20:04:47 +0200

    initial commit

---
+++ src/main/java/com/example/board_game_service/BoardGameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_game_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/board_game_service/BoardGameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_game_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
