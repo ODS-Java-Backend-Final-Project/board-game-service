6c0cf018792db2ef964cf5500744b8bdc7eca17f
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-24 22:33:09 +0200

    añadir api a la ruta

--- src/main/java/com/example/board_game_service/controllers/BoardGameController.java
+++ src/main/java/com/example/board_game_service/controllers/BoardGameController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/board-games")
+@RequestMapping("/api/board-games")
 public class BoardGameController {
     @Autowired
     BoardGameService boardGameService;
