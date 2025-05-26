a6dfe49347211d6b67686e9925b00988736fb523
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-26 16:15:09 +0200

    rutas post, update, getAll pendiente de pruebas postman

--- src/main/java/com/example/board_game_service/controllers/BoardGameController.java
+++ src/main/java/com/example/board_game_service/controllers/BoardGameController.java
@@ -2,13 +2,15 @@ package com.example.board_game_service.controllers;
 
 import com.example.board_game_service.exceptions.BoardGameNotFoundException;
 import com.example.board_game_service.models.BoardGame;
-import com.example.board_game_service.repositories.BoardGameRepository;
 import com.example.board_game_service.services.BoardGameService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/board-games")
@@ -22,9 +24,15 @@ public class BoardGameController {
         try {
             BoardGame foundboardGame = boardGameService.findBoardGameById(id);
             return new ResponseEntity<>(foundboardGame, HttpStatus.OK);
-        } catch (BoardGameNotFoundException e){
+        } catch (BoardGameNotFoundException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<BoardGame> findAllBoardGames() {
+        return boardGameService.findAll();
     }
 
     @DeleteMapping("/{id}")
@@ -32,6 +40,22 @@ public class BoardGameController {
     public void deleteBoardGameById(@PathVariable Long id) {
         try {
             boardGameService.deleteBoardGameById(id);
+        } catch (BoardGameNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BoardGame createBoardGame(@RequestBody BoardGame boardGame) {
+        return boardGameService.saveBoardGame(boardGame);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public BoardGame updateBoardGame(@PathVariable Long id, @RequestBody @Valid BoardGame boardGame) {
+        try {
+            return boardGameService.updateBoardGame(id, boardGame);
         } catch (BoardGameNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
--- src/main/java/com/example/board_game_service/services/BoardGameService.java
+++ src/main/java/com/example/board_game_service/services/BoardGameService.java
@@ -4,8 +4,11 @@ import com.example.board_game_service.exceptions.BoardGameNotFoundException;
 import com.example.board_game_service.models.BoardGame;
 import com.example.board_game_service.repositories.BoardGameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,12 +25,32 @@ public class BoardGameService {
         }
     }
 
+    public List<BoardGame> findAll() {
+        return boardGameRepository.findAll();
+    }
+
     public void deleteBoardGameById(Long id) {
-        Optional<BoardGame> foundBoardGame =   boardGameRepository.findById(id);
+        Optional<BoardGame> foundBoardGame = boardGameRepository.findById(id);
         if (foundBoardGame.isPresent()) {
             boardGameRepository.deleteById(id);
         } else {
             throw new BoardGameNotFoundException("Board game with ID " + id + " not found.");
         }
     }
+
+    public BoardGame saveBoardGame(BoardGame boardGame) {
+        return boardGameRepository.save(boardGame);
+    }
+
+    public BoardGame updateBoardGame(Long id, BoardGame boardGame ) throws BoardGameNotFoundException{
+        BoardGame existingBoardGame = boardGameRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Board game with ID " + id + " not found."));
+        existingBoardGame.setName(boardGame.getName());
+        existingBoardGame.setCategory(boardGame.getCategory());
+        existingBoardGame.setMinPlayers(boardGame.getMinPlayers());
+        existingBoardGame.setMaxPlayers(boardGame.getMaxPlayers());
+        existingBoardGame.setDuration(boardGame.getDuration());
+
+        return boardGameRepository.save(existingBoardGame);
+    }
+
 }
