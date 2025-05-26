6695d25ef651c0ce59ac79fdd664d01daba1ecd9
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-26 13:21:24 +0200

    delete route controller

--- src/main/java/com/example/board_game_service/controllers/BoardGameController.java
+++ src/main/java/com/example/board_game_service/controllers/BoardGameController.java
@@ -7,10 +7,8 @@ import com.example.board_game_service.services.BoardGameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/board-games")
@@ -19,12 +17,23 @@ public class BoardGameController {
     BoardGameService boardGameService;
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<?> getBoardGameById(@PathVariable Long id) {
         try {
             BoardGame foundboardGame = boardGameService.findBoardGameById(id);
             return new ResponseEntity<>(foundboardGame, HttpStatus.OK);
         } catch (BoardGameNotFoundException e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBoardGameById(@PathVariable Long id) {
+        try {
+            boardGameService.deleteBoardGameById(id);
+        } catch (BoardGameNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 }
