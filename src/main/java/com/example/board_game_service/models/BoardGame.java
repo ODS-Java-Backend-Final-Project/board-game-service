7af31a5f08df04acfabe1ca566a05c21fca143ec
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-29 17:42:34 +0200

    validaciones modelo

--- src/main/java/com/example/board_game_service/models/BoardGame.java
+++ src/main/java/com/example/board_game_service/models/BoardGame.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +20,23 @@ public class BoardGame {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name can´t be empty")
+    @Size(max = 100, message = "Name can´t be longer than 100 characters")
     private String name;
+
+    @NotBlank(message = "Category can´t be empty")
     private String category;
+
+    @NotNull(message = "You must provide a minimum number of players")
+    @Min(1)
     private int minPlayers;
+
+    @NotNull(message = "You must provide a maximum number of players")
+    @Min(1)
     private int maxPlayers;
+
+    @NotNull(message = "You must provide a duration in minutes")
+    @Min(1)
     private int duration;
 }
